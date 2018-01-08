680f2636c832aea37c0ee1e3f681a545c2425e48
Theo Rice <theo.rice1996@gmail.com>
2018-01-08 01:12:12 -0500

    added beginning resume page, with link to certificate for angular

---
+++ js/resume.js
@@ -0,0 +1,18 @@
+function hideCertificate(cert, proj) {
+    var x = document.getElementById(cert);
+    if (x.style.display === "none") {
+        x.style.display = "block";
+        x.style.textAlign = "center";
+        // x.style.display = "flex";
+        x.style.margin = "auto";
+        x.style.justifyContent = "center";
+    } else {
+        x.style.display = "none";
+    }
+    var y = document.getElementById(proj);
+    if (y.style.display === "none") {
+        y.style.display = "block";
+    } else {
+        y.style.display = "none";
+    }
+} 
