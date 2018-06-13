951e1e03d34d4f4c042de184bfb7a0f635b6785f
Theo Rice <theo.rice1996@gmail.com>
2018-06-12 21:40:36 -0400

    fixed spelling error and modified centering code to properly work on different displays

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
     $("#moreinfo").on('click', function () {
         $('html, body').animate({
-            scrollTop: $('#expandedAbout').offset().top + $(window).height() / 2
+            scrollTop: $('#section3').offset().top
         }, 3000);
     });
 
